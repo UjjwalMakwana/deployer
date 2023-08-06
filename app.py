2e16e1fa4dace3d0899240f74c80c3185c0f6b6d
UjjwalMakwana <141055743+UjjwalMakwana@users.noreply.github.com>
2023-08-06 12:25:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,14 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-PRIVATE_REPO_URL = os.environ.get ("PRIVATE_REPO_URL")
+PRIVATE_REPO_URL = os.environ.get("PRIVATE_REPO_URL")
 PRIVATE_REPO_TOKEN = os.environ.get("PRIVATE_REPO_TOKEN")
 KOYEB_DEPLOYMENT_PATH = os.environ.get("KOYEB_DEPLOYMENT_PATH")
 
 @app.route('/deploy', methods=['POST'])
 def deploy_to_koyeb():
     try:
-        subprocess.run(['git', 'clone', PRIVATE_REPO_URL, 'tmp_repo'], env={"GIT_ASKPASS": "echo", "GIT_SSH_COMMAND": f"ssh -o StrictHostKeyChecking=no -i {PRIVATE_REPO_TOKEN}"})
-        
-        subprocess.run(['cp', '-r', 'tmp_repo', KOYEB_DEPLOYMENT_PATH])
-        
-        subprocess.run(['rm', '-rf', 'tmp_repo'])
+        subprocess.run(["/bin/bash", "deploy_script.sh"])
         
         return jsonify({"message": "Deployed successfully"})
     except Exception as e:
